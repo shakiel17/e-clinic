661fa49148ac6ce296722570ae11b0f503b602f4
shakiel17 <114818927+shakiel17@users.noreply.github.com>
2024-12-04 14:05:33 +0800

    Update index.php

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -110,11 +110,7 @@
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
                 <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
-              </div>
-              <div class="newsec">
-                  asdfaasdfasdfasdfasdfasddfasdf
-              </div>
-
+              </div>              
             </div>
           </div>
         </div>
